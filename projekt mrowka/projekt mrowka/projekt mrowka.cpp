c0bd51bd23a669c4c8d1a379ce365dcd6492c54a
Szymon <szymon.osicinski@gmail.com>
2016-11-30 17:35:55 +0100

    Almost done

---
+++ projekt mrowka/projekt mrowka/projekt mrowka.cpp
@@ -0,0 +1,80 @@
+// projekt mrowka.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+const int size = 2;
+int tab[size][size] = { 0 };
+int px = size / 2;
+int py = size / 2;
+int direction = 0;
+int steps = 0;
+
+int main()
+{
+	std::cout << "How many steps do you want ant to make \n";
+	std::cin >> steps;
+	for (int b = 0;b < steps;b++) {
+		system("cls");
+		switch (tab[py][px]) {
+		case 1:
+			tab[py][px] = 0;
+			direction = direction - 1;
+			break;
+		case 0:
+			tab[py][px] = 1;
+			direction = direction + 1;
+			break;
+		}
+		switch (direction) {
+		case -1:
+			direction = 3;
+			break;
+		case 4:
+			direction = 0;
+			break;
+		}
+		switch (direction) {
+		case 0:
+			py = py + 1;
+			break;
+		case 1:
+			px = px + 1;
+			break;
+		case 2:
+			py = py - 1;
+			break;
+		case 3:
+			px = px - 1;
+			break;
+		}
+		switch (px) {
+		case -1:
+			px = size - 1;
+			break;
+		case size:
+			px = 0;
+			break;
+		}
+		switch (py) {
+		case -1:
+			py = size - 1;
+			break;
+		case size:
+			px = 0;
+			break;
+		}
+		std::cout << "step: " << b + 1;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		for (int i = 0;i < size;i++) {
+			for (int a = 0;a < size;a++) {
+				std::cout << tab[i][a];
+				std::cout << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+	return 0;
+}
